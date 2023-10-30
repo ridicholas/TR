d094a31d64e0d62f54accdac9cdaa80f6b6e2d68
Nicholas Wolczynski <nicholaswolczynski@Nicholass-MacBook-Pro.local>
2023-10-30 09:58:35 -0500

    full run sh files

--- src/run_cost0.sh
+++ src/run_cost0.sh
@@ -17,26 +17,24 @@
 
 #wait
 
-nohup python3 -u main.py -d 'heart_disease' -i 0 -h 'miscalibrated' -r 'standard' -c 0 -n False -w 'hyrs,brs,tr' > ../logs/log0_heart_miscalibrated_standard_cost0.out &
-nohup python3 -u main.py -d 'heart_disease' -i 1 -h 'miscalibrated' -r 'standard' -c 0 -n False -w 'hyrs,brs,tr' > ../logs/log1_heart_miscalibrated_standard_cost0.out &
-nohup python3 -u main.py -d 'heart_disease' -i 2 -h 'miscalibrated' -r 'standard' -c 0 -n False -w 'hyrs,brs,tr' > ../logs/log2_heart_miscalibrated_standard_cost0.out &
-nohup python3 -u main.py -d 'heart_disease' -i 3 -h 'miscalibrated' -r 'standard' -c 0 -n False -w 'hyrs,brs,tr' > ../logs/log3_heart_miscalibrated_standard_cost0.out &
-nohup python3 -u main.py -d 'heart_disease' -i 4 -h 'miscalibrated' -r 'standard' -c 0 -n False -w 'hyrs,brs,tr' > ../logs/log4_heart_miscalibrated_standard_cost0.out &
-nohup python3 -u main.py -d 'heart_disease' -i 5 -h 'miscalibrated' -r 'standard' -c 0 -n False -w 'hyrs,brs,tr' > ../logs/log5_heart_miscalibrated_standard_cost0.out &
-nohup python3 -u main.py -d 'heart_disease' -i 6 -h 'miscalibrated' -r 'standard' -c 0 -n False -w 'hyrs,brs,tr' > ../logs/log6_heart_miscalibrated_standard_cost0.out &
-nohup python3 -u main.py -d 'heart_disease' -i 7 -h 'miscalibrated' -r 'standard' -c 0 -n False -w 'hyrs,brs,tr' > ../logs/log7_heart_miscalibrated_standard_cost0.out &
-nohup python3 -u main.py -d 'heart_disease' -i 8 -h 'miscalibrated' -r 'standard' -c 0 -n False -w 'hyrs,brs,tr' > ../logs/log8_heart_miscalibrated_standard_cost0.out &
-nohup python3 -u main.py -d 'heart_disease' -i 9 -h 'miscalibrated' -r 'standard' -c 0 -n False -w 'hyrs,brs,tr' > ../logs/log9_heart_miscalibrated_standard_cost0.out &
-nohup python3 -u main.py -d 'heart_disease' -i 10 -h 'miscalibrated' -r 'standard' -c 0 -n False -w 'hyrs,brs,tr' > ../logs/log10_heart_miscalibrated_standard_cost0.out &
-nohup python3 -u main.py -d 'heart_disease' -i 11 -h 'miscalibrated' -r 'standard' -c 0 -n False -w 'hyrs,brs,tr' > ../logs/log11_heart_miscalibrated_standard_cost0.out &
-nohup python3 -u main.py -d 'heart_disease' -i 12 -h 'miscalibrated' -r 'standard' -c 0 -n False -w 'hyrs,brs,tr' > ../logs/log12_heart_miscalibrated_standard_cost0.out &
-nohup python3 -u main.py -d 'heart_disease' -i 13 -h 'miscalibrated' -r 'standard' -c 0 -n False -w 'hyrs,brs,tr' > ../logs/log13_heart_miscalibrated_standard_cost0.out &
-nohup python3 -u main.py -d 'heart_disease' -i 14 -h 'miscalibrated' -r 'standard' -c 0 -n False -w 'hyrs,brs,tr' > ../logs/log14_heart_miscalibrated_standard_cost0.out &
-nohup python3 -u main.py -d 'heart_disease' -i 15 -h 'miscalibrated' -r 'standard' -c 0 -n False -w 'hyrs,brs,tr' > ../logs/log15_heart_miscalibrated_standard_cost0.out &
+#nohup python3 -u main.py -d 'heart_disease' -i 0 -h 'miscalibrated' -r 'standard' -c 0 -n False -w 'hyrs,brs,tr' > ../logs/log0_heart_miscalibrated_standard_cost0.out &
+#nohup python3 -u main.py -d 'heart_disease' -i 1 -h 'miscalibrated' -r 'standard' -c 0 -n False -w 'hyrs,brs,tr' > ../logs/log1_heart_miscalibrated_standard_cost0.out &
+#nohup python3 -u main.py -d 'heart_disease' -i 2 -h 'miscalibrated' -r 'standard' -c 0 -n False -w 'hyrs,brs,tr' > ../logs/log2_heart_miscalibrated_standard_cost0.out &
+#nohup python3 -u main.py -d 'heart_disease' -i 3 -h 'miscalibrated' -r 'standard' -c 0 -n False -w 'hyrs,brs,tr' > ../logs/log3_heart_miscalibrated_standard_cost0.out &
+#nohup python3 -u main.py -d 'heart_disease' -i 4 -h 'miscalibrated' -r 'standard' -c 0 -n False -w 'hyrs,brs,tr' > ../logs/log4_heart_miscalibrated_standard_cost0.out &
+#nohup python3 -u main.py -d 'heart_disease' -i 5 -h 'miscalibrated' -r 'standard' -c 0 -n False -w 'hyrs,brs,tr' > ../logs/log5_heart_miscalibrated_standard_cost0.out &
+#nohup python3 -u main.py -d 'heart_disease' -i 6 -h 'miscalibrated' -r 'standard' -c 0 -n False -w 'hyrs,brs,tr' > ../logs/log6_heart_miscalibrated_standard_cost0.out &
+#nohup python3 -u main.py -d 'heart_disease' -i 7 -h 'miscalibrated' -r 'standard' -c 0 -n False -w 'hyrs,brs,tr' > ../logs/log7_heart_miscalibrated_standard_cost0.out &
+#nohup python3 -u main.py -d 'heart_disease' -i 8 -h 'miscalibrated' -r 'standard' -c 0 -n False -w 'hyrs,brs,tr' > ../logs/log8_heart_miscalibrated_standard_cost0.out &
+#nohup python3 -u main.py -d 'heart_disease' -i 9 -h 'miscalibrated' -r 'standard' -c 0 -n False -w 'hyrs,brs,tr' > ../logs/log9_heart_miscalibrated_standard_cost0.out &
+#nohup python3 -u main.py -d 'heart_disease' -i 10 -h 'miscalibrated' -r 'standard' -c 0 -n False -w 'hyrs,brs,tr' > ../logs/log10_heart_miscalibrated_standard_cost0.out &
+#nohup python3 -u main.py -d 'heart_disease' -i 11 -h 'miscalibrated' -r 'standard' -c 0 -n False -w 'hyrs,brs,tr' > ../logs/log11_heart_miscalibrated_standard_cost0.out &
+#nohup python3 -u main.py -d 'heart_disease' -i 12 -h 'miscalibrated' -r 'standard' -c 0 -n False -w 'hyrs,brs,tr' > ../logs/log12_heart_miscalibrated_standard_cost0.out &
+#nohup python3 -u main.py -d 'heart_disease' -i 13 -h 'miscalibrated' -r 'standard' -c 0 -n False -w 'hyrs,brs,tr' > ../logs/log13_heart_miscalibrated_standard_cost0.out &
+#nohup python3 -u main.py -d 'heart_disease' -i 14 -h 'miscalibrated' -r 'standard' -c 0 -n False -w 'hyrs,brs,tr' > ../logs/log14_heart_miscalibrated_standard_cost0.out &
+#nohup python3 -u main.py -d 'heart_disease' -i 15 -h 'miscalibrated' -r 'standard' -c 0 -n False -w 'hyrs,brs,tr' > ../logs/log15_heart_miscalibrated_standard_cost0.out &
 
-echo 'miscal running'
-wait
-echo 'starting slightly miscal'
+
 
 nohup python3 -u main.py -d 'heart_disease' -i 0 -h 'slightly_miscalibrated' -r 'standard' -c 0 -n False -w 'hyrs,brs,tr' > ../logs/log0_heart_slightly_miscalibrated_standard_cost0.out &
 nohup python3 -u main.py -d 'heart_disease' -i 1 -h 'slightly_miscalibrated' -r 'standard' -c 0 -n False -w 'hyrs,brs,tr' > ../logs/log1_heart_slightly_miscalibrated_standard_cost0.out &
