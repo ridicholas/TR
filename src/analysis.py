82a879b9aded9cc305fe6a0ba82022dfb7fb1415
Nicholas Wolczynski <nicholaswolczynski@Nicholass-MacBook-Pro.local>
2024-05-30 09:54:42 -0500

    analysis

--- src/analysis.py
+++ src/analysis.py
@@ -565,7 +565,7 @@ def cost_validation(rs, val_rs):
     return new_results_means, new_results_stderrs, new_rs
 
 
-costs = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.8, 1.0]
+costs = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5]
 
 num_runs = 5
 datasets = ['heart_disease', 'fico' ,'hr']
