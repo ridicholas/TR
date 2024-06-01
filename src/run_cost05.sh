58324eb096bc544f44e4143b6ae176f04fa1bf1e
Nicholas Wolczynski <nicholaswolczynski@Nicholass-MacBook-Pro.local>
2024-06-01 18:44:29 -0500

    heart5-10

--- src/run_cost05.sh
+++ src/run_cost05.sh
@@ -1,8 +1,8 @@
-nohup python3 -u main.py -d 'heart_disease' -i 5 -h 'offset_05' -r 'standard' -c 0.5 -n False -p '' -b False -w 'hyrs,tr' > ../logs/log0_heart_disease_offset05_standard_cost05.out &
-nohup python3 -u main.py -d 'heart_disease' -i 6 -h 'offset_05' -r 'standard' -c 0.5 -n False -p '' -b False -w 'hyrs,tr' > ../logs/log1_heart_disease_offset05_standard_cost05.out &
-nohup python3 -u main.py -d 'heart_disease' -i 7 -h 'offset_05' -r 'standard' -c 0.5 -n False -p '' -b False -w 'hyrs,tr' > ../logs/log2_heart_disease_offset05_standard_cost05.out &
-nohup python3 -u main.py -d 'heart_disease' -i 8 -h 'offset_05' -r 'standard' -c 0.5 -n False -p '' -b False -w 'hyrs,tr' > ../logs/log3_heart_disease_offset05_standard_cost05.out &
-nohup python3 -u main.py -d 'heart_disease' -i 9 -h 'offset_05' -r 'standard' -c 0.5 -n False -p '' -b False -w 'hyrs,tr' > ../logs/log4_heart_disease_offset05_standard_cost05.out &
+nohup python3 -u main.py -d 'heart_disease' -i 5 -h 'offset_01' -r 'standard' -c 0.5 -n False -p '' -b False -w 'hyrs,tr' > ../logs/log0_heart_disease_offset05_standard_cost05.out &
+nohup python3 -u main.py -d 'heart_disease' -i 6 -h 'offset_01' -r 'standard' -c 0.5 -n False -p '' -b False -w 'hyrs,tr' > ../logs/log1_heart_disease_offset05_standard_cost05.out &
+nohup python3 -u main.py -d 'heart_disease' -i 7 -h 'offset_01' -r 'standard' -c 0.5 -n False -p '' -b False -w 'hyrs,tr' > ../logs/log2_heart_disease_offset05_standard_cost05.out &
+nohup python3 -u main.py -d 'heart_disease' -i 8 -h 'offset_01' -r 'standard' -c 0.5 -n False -p '' -b False -w 'hyrs,tr' > ../logs/log3_heart_disease_offset05_standard_cost05.out &
+nohup python3 -u main.py -d 'heart_disease' -i 9 -h 'offset_01' -r 'standard' -c 0.5 -n False -p '' -b False -w 'hyrs,tr' > ../logs/log4_heart_disease_offset05_standard_cost05.out &
 
 nohup python3 -u main.py -d 'heart_disease' -i 5 -h 'biased' -r 'standard' -c 0.5 -n False -p '' -b False -w 'hyrs,tr' > ../logs/log0_heart_disease_biased_standard_cost05.out &
 nohup python3 -u main.py -d 'heart_disease' -i 6 -h 'biased' -r 'standard' -c 0.5 -n False -p '' -b False -w 'hyrs,tr' > ../logs/log1_heart_disease_biased_standard_cost05.out &
