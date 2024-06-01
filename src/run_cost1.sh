0bada4b506a7619ae1ea04c84e5a16f7d16f55b3
Nicholas Wolczynski <nicholaswolczynski@Nicholass-MacBook-Pro.local>
2024-06-01 12:22:57 -0500

    fico6-1

--- src/run_cost1.sh
+++ src/run_cost1.sh
@@ -1,19 +1,19 @@
-nohup python3 -u main.py -d 'heart_disease' -i 0 -h 'offset_01' -r 'standard' -c 1.0 -n False -p '' -b False -w 'hyrs,tr' > ../logs/log0_hr_offset01_standard_cost1.out &
-nohup python3 -u main.py -d 'heart_disease' -i 1 -h 'offset_01' -r 'standard' -c 1.0 -n False -p '' -b False -w 'hyrs,tr' > ../logs/log1_hr_offset01_standard_cost1.out &
-nohup python3 -u main.py -d 'heart_disease' -i 2 -h 'offset_01' -r 'standard' -c 1.0 -n False -p '' -b False -w 'hyrs,tr' > ../logs/log2_hr_offset01_standard_cost1.out &
-nohup python3 -u main.py -d 'heart_disease' -i 3 -h 'offset_01' -r 'standard' -c 1.0 -n False -p '' -b False -w 'hyrs,tr' > ../logs/log3_hr_offset01_standard_cost1.out &
-nohup python3 -u main.py -d 'heart_disease' -i 4 -h 'offset_01' -r 'standard' -c 1.0 -n False -p '' -b False -w 'hyrs,tr' > ../logs/log4_hr_offset01_standard_cost1.out &
+nohup python3 -u main.py -d 'fico' -i 0 -h 'offset_01' -r 'standard' -c 1.0 -n False -p '' -b False -w 'hyrs,tr' > ../logs/log0_hr_offset01_standard_cost1.out &
+nohup python3 -u main.py -d 'fico' -i 1 -h 'offset_01' -r 'standard' -c 1.0 -n False -p '' -b False -w 'hyrs,tr' > ../logs/log1_hr_offset01_standard_cost1.out &
+nohup python3 -u main.py -d 'fico' -i 2 -h 'offset_01' -r 'standard' -c 1.0 -n False -p '' -b False -w 'hyrs,tr' > ../logs/log2_hr_offset01_standard_cost1.out &
+nohup python3 -u main.py -d 'fico' -i 3 -h 'offset_01' -r 'standard' -c 1.0 -n False -p '' -b False -w 'hyrs,tr' > ../logs/log3_hr_offset01_standard_cost1.out &
+nohup python3 -u main.py -d 'fico' -i 4 -h 'offset_01' -r 'standard' -c 1.0 -n False -p '' -b False -w 'hyrs,tr' > ../logs/log4_hr_offset01_standard_cost1.out &
 
-nohup python3 -u main.py -d 'heart_disease' -i 0 -h 'biased' -r 'standard' -c 1.0 -n False -p '' -b False -w 'hyrs,tr' > ../logs/log0_hr_biased_standard_cost1.out &
-nohup python3 -u main.py -d 'heart_disease' -i 1 -h 'biased' -r 'standard' -c 1.0 -n False -p '' -b False -w 'hyrs,tr' > ../logs/log1_hr_biased_standard_cost1.out &
-nohup python3 -u main.py -d 'heart_disease' -i 2 -h 'biased' -r 'standard' -c 1.0 -n False -p '' -b False -w 'hyrs,tr' > ../logs/log2_hr_biased_standard_cost1.out &
-nohup python3 -u main.py -d 'heart_disease' -i 3 -h 'biased' -r 'standard' -c 1.0 -n False -p '' -b False -w 'hyrs,tr' > ../logs/log3_hr_biased_standard_cost1.out &
-nohup python3 -u main.py -d 'heart_disease' -i 4 -h 'biased' -r 'standard' -c 1.0 -n False -p '' -b False -w 'hyrs,tr' > ../logs/log4_hr_biased_standard_cost1.out &
+nohup python3 -u main.py -d 'fico' -i 0 -h 'biased' -r 'standard' -c 1.0 -n False -p '' -b False -w 'hyrs,tr' > ../logs/log0_hr_biased_standard_cost1.out &
+nohup python3 -u main.py -d 'fico' -i 1 -h 'biased' -r 'standard' -c 1.0 -n False -p '' -b False -w 'hyrs,tr' > ../logs/log1_hr_biased_standard_cost1.out &
+nohup python3 -u main.py -d 'fico' -i 2 -h 'biased' -r 'standard' -c 1.0 -n False -p '' -b False -w 'hyrs,tr' > ../logs/log2_hr_biased_standard_cost1.out &
+nohup python3 -u main.py -d 'fico' -i 3 -h 'biased' -r 'standard' -c 1.0 -n False -p '' -b False -w 'hyrs,tr' > ../logs/log3_hr_biased_standard_cost1.out &
+nohup python3 -u main.py -d 'fico' -i 4 -h 'biased' -r 'standard' -c 1.0 -n False -p '' -b False -w 'hyrs,tr' > ../logs/log4_hr_biased_standard_cost1.out &
 
-nohup python3 -u main.py -d 'heart_disease' -i 0 -h 'biased' -r 'standard' -c 1.0 -n False -p '_dec_bias' -b True -w 'hyrs,tr' > ../logs/log0_hr_biasedDec_standard_cost1.out &
-nohup python3 -u main.py -d 'heart_disease' -i 1 -h 'biased' -r 'standard' -c 1.0 -n False -p '_dec_bias' -b True -w 'hyrs,tr' > ../logs/log1_hr_biasedDec_standard_cost1.out &
-nohup python3 -u main.py -d 'heart_disease' -i 2 -h 'biased' -r 'standard' -c 1.0 -n False -p '_dec_bias' -b True -w 'hyrs,tr' > ../logs/log2_hr_biasedDec_standard_cost1.out &
-nohup python3 -u main.py -d 'heart_disease' -i 3 -h 'biased' -r 'standard' -c 1.0 -n False -p '_dec_bias' -b True -w 'hyrs,tr' > ../logs/log3_hr_biasedDec_standard_cost1.out &
-nohup python3 -u main.py -d 'heart_disease' -i 4 -h 'biased' -r 'standard' -c 1.0 -n False -p '_dec_bias' -b True -w 'hyrs,tr' > ../logs/log4_hr_biasedDec_standard_cost1.out &
+nohup python3 -u main.py -d 'fico' -i 0 -h 'biased' -r 'standard' -c 1.0 -n False -p '_dec_bias' -b True -w 'hyrs,tr' > ../logs/log0_hr_biasedDec_standard_cost1.out &
+nohup python3 -u main.py -d 'fico' -i 1 -h 'biased' -r 'standard' -c 1.0 -n False -p '_dec_bias' -b True -w 'hyrs,tr' > ../logs/log1_hr_biasedDec_standard_cost1.out &
+nohup python3 -u main.py -d 'fico' -i 2 -h 'biased' -r 'standard' -c 1.0 -n False -p '_dec_bias' -b True -w 'hyrs,tr' > ../logs/log2_hr_biasedDec_standard_cost1.out &
+nohup python3 -u main.py -d 'fico' -i 3 -h 'biased' -r 'standard' -c 1.0 -n False -p '_dec_bias' -b True -w 'hyrs,tr' > ../logs/log3_hr_biasedDec_standard_cost1.out &
+nohup python3 -u main.py -d 'fico' -i 4 -h 'biased' -r 'standard' -c 1.0 -n False -p '_dec_bias' -b True -w 'hyrs,tr' > ../logs/log4_hr_biasedDec_standard_cost1.out &
 
 
