abcd9d071ae756de2b579a8d0714f1cbc9dbbfd7
Nicholas Wolczynski <nicholaswolczynski@Nicholass-MacBook-Pro.local>
2024-06-25 12:04:24 -0400

    discretion0

--- src/discretion_run.py
+++ src/discretion_run.py
@@ -2,7 +2,7 @@ from run import *
 import sys, getopt
 
 #making sure wd is file directory so hardcoded paths work
-#os.chdir("..")
+os.chdir("..")
 
 
 run('heart_disease', 0, 'biased', runtype='standard', which_models=['tr,hyrs'], contradiction_reg=0.0, remake_humans=True, human_decision_bias=False, custom_name='_discretionTrue', use_true=True, subsplit=1, shared_human=True)
