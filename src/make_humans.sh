37ef7f875915efbebd838d149ecd87a768b0d6f4
Nicholas Wolczynski <nicholaswolczynski@Nicholass-MacBook-Pro.local>
2024-09-05 10:33:17 -0500

    hd 10-20

--- src/make_humans.sh
+++ src/make_humans.sh
@@ -9,6 +9,17 @@ nohup python3 -u main.py -d 'heart_disease' -i 17 -h 'offset_01' -r 'standard' -
 nohup python3 -u main.py -d 'heart_disease' -i 18 -h 'offset_01' -r 'standard' -c 0 -n True -p '' -b False -w '' > ../logs/log18_heart_disease_offset01_standard_nomodels.out &
 nohup python3 -u main.py -d 'heart_disease' -i 19 -h 'offset_01' -r 'standard' -c 0 -n True -p '' -b False -w '' > ../logs/log19_heart_disease_offset01_standard_nomodels.out &
 
+nohup python3 -u main.py -d 'heart_disease' -i 0 -h 'biased' -r 'standard' -c 0 -n True -p '_dec_bias' -b True -w '' > ../logs/log0_heart_disease_biased_dec_bias_standard_nomodels.out &
+nohup python3 -u main.py -d 'heart_disease' -i 1 -h 'biased' -r 'standard' -c 0 -n True -p '_dec_bias' -b True -w '' > ../logs/log1_heart_disease_biased_dec_bias_standard_nomodels.out &
+nohup python3 -u main.py -d 'heart_disease' -i 2 -h 'biased' -r 'standard' -c 0 -n True -p '_dec_bias' -b True -w '' > ../logs/log2_heart_disease_biased_dec_bias_standard_nomodels.out &
+nohup python3 -u main.py -d 'heart_disease' -i 3 -h 'biased' -r 'standard' -c 0 -n True -p '_dec_bias' -b True -w '' > ../logs/log3_heart_disease_biased_dec_bias_standard_nomodels.out &
+nohup python3 -u main.py -d 'heart_disease' -i 4 -h 'biased' -r 'standard' -c 0 -n True -p '_dec_bias' -b True -w '' > ../logs/log4_heart_disease_biased_dec_bias_standard_nomodels.out &
+nohup python3 -u main.py -d 'heart_disease' -i 5 -h 'biased' -r 'standard' -c 0 -n True -p '_dec_bias' -b True -w '' > ../logs/log5_heart_disease_biased_dec_bias_standard_nomodels.out &
+nohup python3 -u main.py -d 'heart_disease' -i 6 -h 'biased' -r 'standard' -c 0 -n True -p '_dec_bias' -b True -w '' > ../logs/log6_heart_disease_biased_dec_bias_standard_nomodels.out &
+nohup python3 -u main.py -d 'heart_disease' -i 7 -h 'biased' -r 'standard' -c 0 -n True -p '_dec_bias' -b True -w '' > ../logs/log7_heart_disease_biased_dec_bias_standard_nomodels.out &
+nohup python3 -u main.py -d 'heart_disease' -i 8 -h 'biased' -r 'standard' -c 0 -n True -p '_dec_bias' -b True -w '' > ../logs/log8_heart_disease_biased_dec_bias_standard_nomodels.out &
+nohup python3 -u main.py -d 'heart_disease' -i 9 -h 'biased' -r 'standard' -c 0 -n True -p '_dec_bias' -b True -w '' > ../logs/log9_heart_disease_biased_dec_bias_standard_nomodels.out &
+
 nohup python3 -u main.py -d 'heart_disease' -i 10 -h 'biased' -r 'standard' -c 0 -n True -p '_dec_bias' -b True -w '' > ../logs/log10_heart_disease_biased_dec_bias_standard_nomodels.out &
 nohup python3 -u main.py -d 'heart_disease' -i 11 -h 'biased' -r 'standard' -c 0 -n True -p '_dec_bias' -b True -w '' > ../logs/log11_heart_disease_biased_dec_bias_standard_nomodels.out &
 nohup python3 -u main.py -d 'heart_disease' -i 12 -h 'biased' -r 'standard' -c 0 -n True -p '_dec_bias' -b True -w '' > ../logs/log12_heart_disease_biased_dec_bias_standard_nomodels.out &
